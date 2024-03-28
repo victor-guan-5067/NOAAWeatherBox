e93808c43cfc9caf754fdaf70d69f99742933b45
Victor Guan <vguan12@gmail.com>
2024-03-28 15:46:34 -0700

    rm whitespace

--- Categories.py
+++ Categories.py
@@ -15,74 +15,74 @@ months = {
     }
 
 header = '''{{{{Weather box
- | width       = auto
- | collapsed   = yes
- | single line = yes
- | location    = {}, {} (1991–2020 normals, extremes –)
- '''
+| width       = auto
+| collapsed   = yes
+| single line = yes
+| location    = {}, {} (1991–2020 normals, extremes –)
+'''
 
 record_highs = '''| Jan record high F  = 
- | Feb record high F  = 
- | Mar record high F  = 
- | Apr record high F  = 
- | May record high F  = 
- | Jun record high F  = 
- | Jul record high F  = 
- | Aug record high F  = 
- | Sep record high F  = 
- | Oct record high F  = 
- | Nov record high F  = 
- | Dec record high F  = 
- | year record high F = 
+| Feb record high F  = 
+| Mar record high F  = 
+| Apr record high F  = 
+| May record high F  = 
+| Jun record high F  = 
+| Jul record high F  = 
+| Aug record high F  = 
+| Sep record high F  = 
+| Oct record high F  = 
+| Nov record high F  = 
+| Dec record high F  = 
+| year record high F = 
 '''
 
-record_lows = ''' | Jan record low F  = 
- | Feb record low F  = 
- | Mar record low F  = 
- | Apr record low F  = 
- | May record low F  = 
- | Jun record low F  = 
- | Jul record low F  = 
- | Aug record low F  = 
- | Sep record low F  = 
- | Oct record low F  = 
- | Nov record low F  = 
- | Dec record low F  = 
- | year record low F = 
+record_lows = '''| Jan record low F  = 
+| Feb record low F  = 
+| Mar record low F  = 
+| Apr record low F  = 
+| May record low F  = 
+| Jun record low F  = 
+| Jul record low F  = 
+| Aug record low F  = 
+| Sep record low F  = 
+| Oct record low F  = 
+| Nov record low F  = 
+| Dec record low F  = 
+| year record low F = 
 '''
 
-precip_days = ''' | unit precipitation days = 0.01 in
- | Jan precipitation days  = 
- | Feb precipitation days  = 
- | Mar precipitation days  = 
- | Apr precipitation days  = 
- | May precipitation days  = 
- | Jun precipitation days  = 
- | Jul precipitation days  = 
- | Aug precipitation days  = 
- | Sep precipitation days  = 
- | Oct precipitation days  = 
- | Nov precipitation days  = 
- | Dec precipitation days  = 
- | year precipitation days = 
+precip_days = '''| unit precipitation days = 0.01 in
+| Jan precipitation days  = 
+| Feb precipitation days  = 
+| Mar precipitation days  = 
+| Apr precipitation days  = 
+| May precipitation days  = 
+| Jun precipitation days  = 
+| Jul precipitation days  = 
+| Aug precipitation days  = 
+| Sep precipitation days  = 
+| Oct precipitation days  = 
+| Nov precipitation days  = 
+| Dec precipitation days  = 
+| year precipitation days = 
 '''
-snow_days = ''' | unit snow days = 0.1 in
- | Jan snow days  = 
- | Feb snow days  = 
- | Mar snow days  = 
- | Apr snow days  = 
- | May snow days  = 
- | Jun snow days  = 
- | Jul snow days  = 
- | Aug snow days  = 
- | Sep snow days  = 
- | Oct snow days  = 
- | Nov snow days  = 
- | Dec snow days  = 
- | year snow days = 
+snow_days = '''| unit snow days = 0.1 in
+| Jan snow days  = 
+| Feb snow days  = 
+| Mar snow days  = 
+| Apr snow days  = 
+| May snow days  = 
+| Jun snow days  = 
+| Jul snow days  = 
+| Aug snow days  = 
+| Sep snow days  = 
+| Oct snow days  = 
+| Nov snow days  = 
+| Dec snow days  = 
+| year snow days = 
 '''
 
-footer = ''' | source = [[National Oceanic and Atmospheric Administration|NOAA]]<ref name="NOWData">{{{{cite web
+footer = '''| source = [[National Oceanic and Atmospheric Administration|NOAA]]<ref name="NOWData">{{{{cite web
 |url = {nowdata_url}
 |title = NOWData - NOAA Online Weather Data
 |publisher = National Oceanic and Atmospheric Administration
